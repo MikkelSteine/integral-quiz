a84de9d21d7e2c3144104e9e43b1857198809e00
Mikkel Steine <mikkel@integral.no>
2023-04-23 23:00:09 +0200

    Legger til eksempel på frackend-endepunkt

---
+++ pages/api/quiz.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Quiz = {
+  markers: [{ question: string }]
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Quiz>
+) {
+  res.status(200).json({ markers: [] });
+}
